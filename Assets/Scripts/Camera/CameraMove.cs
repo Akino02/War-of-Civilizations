b483c4dc0684b77760c10443558ead971561ccb6
Michal Adámek <romster2012@seznam.cz>
2024-04-07 18:41:13 +0200

    Update_059
    Pohyb kamery myší

--- Assets/Scripts/Camera/CameraMove.cs
+++ Assets/Scripts/Camera/CameraMove.cs
@@ -9,6 +9,8 @@ public class CameraMove : MonoBehaviour
 	private float activeX;                                      //promenna pro ulozeni zda se hybe ci ne
 	public float movespeed = 3;                                 //rychlost pohybu objektu
 
+    private int touchField = 10;                                //velikost pole pro mys, po najeti do pole se bude tam pohybovat kamera
+
 	//promenne pro border kamery
     float borderLposX;
     public GameObject playerBase;
@@ -35,9 +37,30 @@ public class CameraMove : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-        if (!LogScript.isGameOver)
+        //Debug.Log(Input.mousePosition.x);
+        Debug.Log(Input.mousePosition.y);
+        //Debug.Log(Screen.width);
+        /*if (!LogScript.isGameOver)
         {
             activeX = Input.GetAxis("Horizontal");                  //definice pohybu(ten pohyb je urcen pro A, D a taky pro levou sipku a pravou sipku)
+        }*/
+        if (!LogScript.isGameOver)
+        {
+            //pohyb pomoci myse
+            if (Input.mousePosition.x < Screen.width / touchField && Input.mousePosition.y < (Screen.height * 3) / 4 && Input.mousePosition.y > Screen.height/ touchField)                        //pokud je mys left
+            {
+                activeX = -1;
+            }
+            else if (Input.mousePosition.x > Screen.width - Screen.width / touchField && Input.mousePosition.y < (Screen.height * 3) / 4 && Input.mousePosition.y > Screen.height / touchField)   //pokud je mys right
+            {
+                activeX = 1;
+            }
+            //pohyb pomoci klavesnice
+            else
+            {
+                //definice pohybu(ten pohyb je urcen pro A, D a taky pro levou sipku a pravou sipku)
+                activeX = Input.GetAxis("Horizontal");
+            }
         }
         rb.velocity = new Vector2((movespeed * activeX), rb.velocity.y);                                                       //pohyb kamery
         sun.transform.position = new Vector3(widthFromSun.transform.position.x + transform.position.x, sun.transform.position.y, sun.transform.position.z);     //nastaveni pozice pro slunce
